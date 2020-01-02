327c1ae87f7ec546fdde1a43a8faab2027442637
KonradMagiera <k.magiera1@wp.pl>
2020-01-02 09:50:19 +0100

    experimental push to database (item has random generated name/id)

--- ipam/src/components/layout/Home.js
+++ ipam/src/components/layout/Home.js
@@ -2,18 +2,26 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { login } from '../../actions/authentication'
+import  Firebase  from '../../firebaseConfig';
 
 
 const mapDispatchToProps = dispatch => ({
   logout: isAuth => dispatch(login(isAuth))
 })
 
+function db(){
+  //Firebase.database().ref('/ips').push(["new", "smth"]).
+  var key = Firebase.database().ref('/ips').push({"ip:":"192", "name":"test"}).key //dodaje ale pod jakims magicznym kluczem
+ // Firebase.database().ref('/devices').set({ "a":"test", "b":"xd", "c":"dx"}); //nadpisuje cala baze i wstawia tylko to
+}
+
+
 function Home(props)  {
   return (
     <div>
         <label>After login TMP TEST</label>
         <div>
-        <Link to="/">Link without logout</Link>
+        <Link to="/" onClick={() => { db() }}>Link without logout</Link>
         </div>
         <div>
         <Link to="/" onClick={() => { props.logout(false) }} >
